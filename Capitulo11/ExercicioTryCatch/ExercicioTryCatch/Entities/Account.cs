cea0404476bb750b0e5287a3b00254e8b2ccd5f7
Diego Oliveira <dipeoliver@outlook.com>
2021-09-08 15:36:32 -0300

    Update to chapter 11

---
+++ Capitulo11/ExercicioTryCatch/ExercicioTryCatch/Entities/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using ExercicioTryCatch.Entities.DomainExcepetion;
+
+namespace ExercicioTryCatch.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit ( double amount)
+        {
+            if (amount > 0)
+            {
+                throw new DomainException("The amount must be greater than 0 reais ");
+            }
+
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The Amount exceeds withdraw limit");
+            } 
+
+            if (amount > Balance && Balance > 0)
+            {
+                throw new DomainException("The Amount exceeds the Balance");
+            }
+
+            Balance -= amount;
+        }       
+    }
+}
---
+++ Capitulo11/ExercicioTryCatch/ExercicioTryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExercicioTryCatch.Entities;
+using ExercicioTryCatch.Entities.DomainExcepetion;
+using System;
+using System.Globalization;
+
+namespace ExercicioTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balnace: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("WithDraw limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                account = new Account(number, holder, initialBalance, withDrawLimit);
+
+                Console.Write("Enter amount for witdraw: ");
+                double witDraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.WithDraw(witDraw);
+                Console.WriteLine($"New Balance: {account.Balance.ToString("F2",CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }      
+        }
+    }
+}
